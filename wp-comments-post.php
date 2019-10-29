3c4a4ab36d2360d91a6fe6595c15b5e6d642b33c
gbenga <gbengar36@gmail.com>
2019-10-29 05:30:25 -0700

    first modification

--- wp-comments-post.php
+++ wp-comments-post.php
@@ -5,7 +5,7 @@
  * @package WordPress
  */
 
-if ( 'POST' != $_SERVER['REQUEST_METHOD'] ) {
+if ( 'POST' == $_SERVER['REQUEST_METHOD'] ) {
 	$protocol = $_SERVER['SERVER_PROTOCOL'];
 	if ( ! in_array( $protocol, array( 'HTTP/1.1', 'HTTP/2', 'HTTP/2.0' ) ) ) {
 		$protocol = 'HTTP/1.0';
